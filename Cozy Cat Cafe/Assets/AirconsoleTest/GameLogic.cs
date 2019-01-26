9971477bc76ef09e03bebf94e6c11398f4d673f4
VeryBester <david15202@gmail.com>
2019-01-26 12:13:06 -0600

    Added airconsole

---
+++ Cozy Cat Cafe/Assets/AirconsoleTest/GameLogic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class GameLogic : MonoBehaviour
+{
+     void Awake() {
+        AirConsole.instance.onMessage += OnMessage;    
+     }
+
+     void OnMessage(int fromDeviceID, JToken data){
+         Debug.Log("message from " + fromDeviceID + ", data: " + data);
+         if(data["action"] != null && data["action"].ToString().Equals("interact")){
+             Camera.main.backgroundColor = new Color (Random.Range (0f, 1f), Random.Range (0f, 1f), Random.Range(0f, 1f));
+         }
+     }
+
+    void OnDestroy() {
+        if(AirConsole.instance != null){
+            AirConsole.instance.onMessage -= OnMessage;
+        }         
+     }
+}

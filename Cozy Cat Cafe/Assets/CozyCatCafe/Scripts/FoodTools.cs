a5005aebb07cfd395e2a61c6562a5668718eba4d
robertyguan <33498270+robertyguan@users.noreply.github.com>
2019-01-26 14:14:51 -0600

    Update FoodTools.cs

--- Cozy Cat Cafe/Assets/CozyCatCafe/Scripts/FoodTools.cs
+++ Cozy Cat Cafe/Assets/CozyCatCafe/Scripts/FoodTools.cs
@@ -7,12 +7,15 @@ public class FoodTools : MonoBehaviour
 {
     Food foodProcessing;
     Dictionary<Food,Food> myFoods;
+    PlayerStats player;
+    bool foodReady;
 
     // Start is called before the first frame update
     void Start()
     {
         myFoods = new Dictionary<Food,Food>();
         foodProcessing = null;
+        foodReady = false;
     }
 
     // Update is called once per frame
@@ -22,17 +25,26 @@ public class FoodTools : MonoBehaviour
     }
 
     void OnMouseDown(){
-        getResult();
+        if(foodProcessing == null){
+            putFood();
+        } else{
+            if(foodReady){
+                getFood();
+                foodReady = false;
+            }
+        }
     }
 
-    Food getResult(){
-        // if(myFoods[PlayerStats.foodCurrentlyHolding] == null || foodProcessing != null){
-
-        // } else{
-        //     foodProcessing = PlayerStats.foodCurrentlyHolding;
-
-        //     PlayerStats.foodCurrentlyHolding = myFoods[PlayerStats.foodCurrentlyHolding];
-        // }
-        return null;
+    void putFood(){
+        if(myFoods[player.foodCurrentlyHolding] != null){
+            foodProcessing = player.foodCurrentlyHolding;
+            player.foodCurrentlyHolding = null;
+        }
+    }
+    void getFood(){
+        if(player.foodCurrentlyHolding == null){
+            player.foodCurrentlyHolding = myFoods[foodProcessing];
+            foodProcessing = null;
+        }
     }
 }

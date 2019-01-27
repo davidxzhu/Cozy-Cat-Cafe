8581be301a428abce164a294100b0bb347d66f94
VeryBester <david15202@gmail.com>
2019-01-27 12:19:53 -0600

    Added HoverOverFood display

---
+++ Cozy Cat Cafe/Assets/CozyCatCafe/Scripts/MouseFoodHover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CozyCatCafe.Scripts;
+
+public class MouseFoodHover : MonoBehaviour
+{
+    public Transform foodSource;
+    public Sprite foodDisplay;
+    public DialogueBubble bubble;
+
+    private void OnMouseEnter() {
+
+        bubble.ChangeSprite(foodDisplay); 
+    }
+
+    private void OnMouseExit() {
+
+        bubble.ChangeSprite(null);
+    }
+}

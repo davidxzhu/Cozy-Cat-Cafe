23e2201dd6d37251317598cf507783fddd52fd27
GahwonLee <creativitRy@gmail.com>
2019-01-27 12:44:07 -0600

    sample scene

--- Cozy Cat Cafe/Assets/CozyCatCafe/Scripts/Shop/ShopItem.cs
+++ Cozy Cat Cafe/Assets/CozyCatCafe/Scripts/Shop/ShopItem.cs
@@ -7,6 +7,8 @@ namespace CozyCatCafe.Scripts.Shop
 	[CreateAssetMenu(menuName = "Shop Item")]
 	public class ShopItem : ScriptableObject
 	{
+		public string Description;
+		
 		public Sprite Thumbnail;
 
 		public int Cost = 1;

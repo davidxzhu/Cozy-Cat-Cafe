f7a4fd4c55315e8d57eddb844a7552a75f93fc91
GahwonLee <creativitRy@gmail.com>
2019-01-26 21:29:02 -0600

    seat

---
+++ Cozy Cat Cafe/Assets/CozyCatCafe/Scripts/CustomerSpawner.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace CozyCatCafe.Scripts
+{
+	public class CustomerSpawner : MonoBehaviour
+	{
+		public Seat[] Seats;
+		public Customers CustomerPrefab;
+
+		public float MinDuration = 1f;
+		public float MaxDuration = 5f;
+
+		private float _remainingDuration = 2f;
+
+		private void Update()
+		{
+			_remainingDuration -= Time.deltaTime;
+			if (_remainingDuration < 0f)
+			{
+				_remainingDuration = Random.Range(MinDuration, MaxDuration);
+
+				for (var i = 0; i < Seats.Length; i++)
+				{
+					if (Seats[i].Customer == null)
+					{
+						var obj = Instantiate(CustomerPrefab, transform.position, Quaternion.identity);
+						obj.setSeat(Seats[i]);
+					}
+				}
+			}
+		}
+	}
+}
--- Cozy Cat Cafe/Assets/CozyCatCafe/Scripts/Customers.cs
+++ Cozy Cat Cafe/Assets/CozyCatCafe/Scripts/Customers.cs
@@ -1,39 +1,46 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using CozyCatCafe.Scripts;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class Customers : MonoBehaviour
 {
     private State state;
 
     public Transform customer;
     public Transform dish;
-    public Transform seat;
+    public Seat seat;
     public float customerMoveSpeed;
 
     [Header("Food")]
     public DialogueBubble bubble;
 
     public Food orderDish;
+    [NonSerialized]
     public bool gotFood;
     private Vector3 moveStep;
     public float fadeStep;
     private float currentOpacity = 0;
-    //private float currentTime = 0f;
+    
+    private SpriteRenderer _spriteRenderer;
+
     private void Awake() {
         //createDish();
-        GetComponent<SpriteRenderer>().color = new Color(1,1,1,currentOpacity);
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.color = new Color(1,1,1,currentOpacity);
 
         //Used for testing
-        moveStep = (customer.position - seat.position) / customerMoveSpeed;
+        if (seat != null)
+            moveStep = (customer.position - seat.transform.position) / customerMoveSpeed;
         //fadeStep = 1 / Vector3.Distance(customer.position, seat.position);
 
     }
 
     private void Update() {
         if (state == State.WalkingIn) {
-            if (Vector3.Distance(customer.position, seat.position) > 0.1)
+            if (Vector3.Distance(customer.position, seat.transform.position) > 0.1)
                 goToSeat();
             else {
                 state = State.Ordering;
@@ -52,6 +59,7 @@ public class Customers : MonoBehaviour
         
         else if (state == State.Eating) {
             state = State.WalkingOut;
+            seat.Customer = null;
         } 
         
         else {
@@ -60,16 +68,17 @@ public class Customers : MonoBehaviour
     }
 
     // Used for Spawner
-    void setSeat(Transform seat){
+    public void setSeat(Seat seat){
         this.seat = seat;
-        moveStep = (seat.position - customer.position) / customerMoveSpeed;
+        seat.Customer = this;
+        moveStep = (seat.transform.position - customer.position) / customerMoveSpeed;
         fadeStep = 1 / customerMoveSpeed;//Vector3.Distance(customer.position, seat.position);
     }
 
     void goToSeat(){
         customer.position = customer.position - moveStep;
         currentOpacity += 1/fadeStep;
-        GetComponent<SpriteRenderer>().color = new Color(1,1,1,currentOpacity);
+        _spriteRenderer.color = new Color(1,1,1,currentOpacity);
     }
 
     void createDish(){
@@ -79,7 +88,7 @@ public class Customers : MonoBehaviour
     void leave(){
         customer.position = customer.position + moveStep;
         currentOpacity -= 1/fadeStep;
-        GetComponent<SpriteRenderer>().color = new Color(1,1,1,currentOpacity);
+        _spriteRenderer.color = new Color(1,1,1,currentOpacity);
 
         if(currentOpacity < 0){
             Destroy(gameObject);

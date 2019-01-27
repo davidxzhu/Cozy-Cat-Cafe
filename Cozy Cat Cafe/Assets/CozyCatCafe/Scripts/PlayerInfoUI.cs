2e9269817705207d3f575aa827cd88fce688e746
VeryBester <david15202@gmail.com>
2019-01-27 10:44:45 -0600

    Added PlayerInfo UI

---
+++ Cozy Cat Cafe/Assets/CozyCatCafe/Scripts/PlayerInfoUI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using CozyCatCafe.Scripts;
+
+public class PlayerInfoUI : MonoBehaviour
+{
+    public PlayerStats PlayerInfo;
+    public Image CurrentFoodDisplay;
+    public Text CurrentMoneyDisplay;
+    private Food CurrentHolding;
+    private int CurrentMoney;
+
+    // Update is called once per frame
+    private void Awake() {
+        CurrentMoney = PlayerInfo.Money;
+        CurrentMoneyDisplay.text = CurrentMoney.ToString();
+    }
+    void Update()
+    {
+        if(CurrentMoney != PlayerInfo.Money){
+            CurrentMoney = PlayerInfo.Money;
+            CurrentMoneyDisplay.text = CurrentMoney.ToString();
+        }
+
+
+        if(CurrentHolding != PlayerInfo.holding){
+            
+            if(PlayerInfo.holding != null){
+                CurrentFoodDisplay.sprite = PlayerInfo.holding.Sprite;
+            }
+            else{
+                CurrentFoodDisplay.sprite = null;
+            }
+            CurrentHolding = PlayerInfo.holding;
+        }
+
+        
+    }
+}
